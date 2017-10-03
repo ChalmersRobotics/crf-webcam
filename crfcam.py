0c672f4bd1bc005d91460bfeed8b4fb3308c8279
Tim Gremalm <tim@gremalm.se>
2017-10-03 19:40:17 +0200

    Brake out functions for handling images

---
+++ crfcam.py
@@ -0,0 +1,12 @@
+class crfcam:
+	def detectMovement(self, last, current, blobsOutput=None):
+		return(False)
+
+	def anonymize(self, img):
+		print("hej")
+		return(img)
+
+	def drawCRFHeader(self, name, img):
+		return(img)
+
+
