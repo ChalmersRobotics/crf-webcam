2aede6089a4673466c3f2fccc9c199e79bc27901
Tim Gremalm <tim@gremalm.se>
2017-10-06 05:20:53 +0200

    Mockup of camera-motion-publish flow

--- main.py
+++ main.py
@@ -9,3 +9,31 @@ picam = picam2cv()
 
 import crf
 
+last = Image()
+current = picam.getRaspiCamImage()
+
+motionDetected, blobsDetected = crf.detectMovement(last, current)
+numberOfPeople, currentAnon = crf.anonymize(current)
+
+roomName = config['name']
+sDate = Now(ISO8601)
+sDateTime = Now(ISO8601)
+
+filenameCurrent = '{}_current_{}.jpg'.format(roomName, sDate)
+filenameDetect = '{}_detect_{}.jpg'.format(roomName, sDate)
+titleCurrent = 'CRF {} {}'.format(roomName, sDateTime)
+titleDetect = 'CRF {} {}.jpg'.format(roomName, sDateTime)
+
+filesToUpload = []
+imgCurrent = crf.drawText(currentAnon, titleCurrent)
+imgCurrent.save(filenameCurrent)
+filesToUpload.append(filenameCurrent)
+
+if motionDetected:
+	imgMovement = crf.drawMovement(currentAnon, blobsDetected)
+	imgMovement = imgMovement(imgMovement, titleDetect)
+	imgMovement.save(filenameDetect)
+	filesToUpload.append(filenameDetect)
+
+crf.sendSFTP(filesToUpload)
+
