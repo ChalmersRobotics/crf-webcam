a799c6198fe03381488d74976a95ec45deff8bd1
Tim Gremalm <tim@gremalm.se>
2017-11-09 01:00:55 +0100

    Cam thread, sigint handling

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-import sys
+import sys, signal, threading
+
 sys.path.append('SimpleCVHelper')
 from SimpleCVHelper import *
 
@@ -16,30 +17,54 @@ picam = picam2cv()
 import crf
 
 last = Image()
-current = picam.getRaspiCamImage()
+current = Image()
+shutdown = False
 
-motionDetected, blobsDetected = crf.detectMovement(last, current)
-numberOfPeople, currentAnon = crf.anonymize(current)
+def threadCamLoop():
+	while not shutdown:
+		current = picam.getRaspiCamImage()
 
-roomName = config['name']
-sDate = Now(ISO8601)
-sDateTime = Now(ISO8601)
+		motionDetected, blobsDetected = crf.detectMovement(last, current)
+		numberOfPeople, currentAnon = crf.anonymize(current)
 
-filenameCurrent = config['filenameNow'].format(sDate)
-filenameDetect = config['filenameMovement'].format(sDate)
-titleCurrent = config['titleNow'].format(sDateTime)
-titleDetect = config['titleMovement'].format(sDateTime)
+		roomName = config['name']
+		sDate = Now(ISO8601)
+		sDateTime = Now(ISO8601)
 
-filesToUpload = []
-imgCurrent = crf.drawText(currentAnon, titleCurrent)
-imgCurrent.save(filenameCurrent)
-filesToUpload.append(filenameCurrent)
+		filenameCurrent = config['filenameNow'].format(sDate)
+		filenameDetect = config['filenameMovement'].format(sDate)
+		titleCurrent = config['titleNow'].format(sDateTime)
+		titleDetect = config['titleMovement'].format(sDateTime)
 
-if motionDetected:
-	imgMovement = crf.drawMovement(currentAnon, blobsDetected)
-	imgMovement = imgMovement(imgMovement, titleDetect)
-	imgMovement.save(filenameDetect)
-	filesToUpload.append(filenameDetect)
+		filesToUpload = []
+		imgCurrent = crf.drawText(currentAnon, titleCurrent)
+		imgCurrent.save(filenameCurrent)
+		filesToUpload.append(filenameCurrent)
 
-crf.sendSFTP(filesToUpload)
+		if motionDetected:
+			imgMovement = crf.drawMovement(currentAnon, blobsDetected)
+			imgMovement = imgMovement(imgMovement, titleDetect)
+			imgMovement.save(filenameDetect)
+			filesToUpload.append(filenameDetect)
+
+		crf.sendSFTP(filesToUpload)
+
+def signal_handler(signal, frame):
+	print('SIGINT detected. Prepareing to shut down.')
+	global shutdown
+		shutdown = True
+
+if __name__ == '__main__':
+	print("Starting cam")
+
+	#Start listening for SIGINT (Ctrl+C)
+	signal.signal(signal.SIGINT, signal_handler)
+
+	t = threading.Thread(target=threadCamLoop, args=())
+	t.start()
+
+	#Cause the process to sleep until a signal is received
+	signal.pause()
+
+	sys.exit(0)
 
